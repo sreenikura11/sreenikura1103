89458f5fddcefc2b9b4513219596a28bd3b37f75
sreenikura11.github.io <SE22UCSE258>
2025-06-10 14:56:12 +0530

    new19

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -3,5 +3,21 @@ import { AppContext } from "./App";
 import { useContext } from "react";
 export default function Cart() {
   const { cart } = useContext(AppContext);
-  return <div>{cart.name}</div>;
+  const increment = () => {
+    setCart({ ...Cart, qty: cart[qty] + 1});
+
+
+  };
+
+  const decrement = () => {
+   setCart({ ...cart, qty: cart[qty] - 1})
+
+  }
+  return (
+    <div>
+        <h1>My Cart</h1>
+    </div>
+  )
+
+  
 }
