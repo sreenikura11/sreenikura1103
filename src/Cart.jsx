56da89a8b0a3359b4bbac40856ef7c81efbd7a14
sreenikura11.github.io <SE22UCSE258>
2025-06-10 14:47:37 +0530

    yo11

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-import ( AppContext ) from './App'
-import ( useContext ) from 'react'
-export default function Cart(){
-    return (
-        <div>Cart</div>
-    )
-       
+import React from "react";
+import { AppContext } from "./App";
+import { useContext } from "react";
+export default function Cart() {
+  const { cart } = useContext(AppContext);
+  return <div>{cart.name}</div>;
 }
